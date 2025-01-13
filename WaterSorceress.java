db184e16e037ea498159d256a14a5cf6b5b28950
Andy Li <ali60@stuy.edu>
2025-01-12 19:49:00 -0500

    finished the rest of the watersorceress constructors and the methods related to special (dewdrops)

--- WaterSorceress.java
+++ WaterSorceress.java
@@ -1,9 +1,39 @@
 import java.util.*;
 public class WaterSorceress extends Adventurer{
-  public FireMage(String name, int hp, String language){
+  int dewdrops, dewdropsMax;
+  public WaterSorceress(String name, int hp){
     super(name, hp);
     dewdropsMax = 12;
     dewdrops = 0;
-    preferredLanguage = language;
   }
+
+  public WaterSorceress(String name){
+    this(name, 30);
+  }
+
+  public WaterSorceress(){
+    this("Primarina");
+  }
+
+  public String getSpecialName(){
+    return "dewdrops";
+  }
+
+  public int getSpecial(){
+    return dewdrops;
+  }
+
+  public int getSpecialMax(){
+    return dewdropsMax;
+  }
+
+  public void setSpecial(int n){
+    if (n < rageMax){
+      dewdrops = n;
+    }
+    else{
+      dewdrops = rageMax;
+    }
+  }
+
 }
