96348d1d1e2c8699b6f0233d07464167f0a313c4
Henry Santos Hendricks <henrys8913@nycstudents.net>
2025-01-14 22:00:37 -0500

    added group attack functon

--- ElectroBoss.java
+++ ElectroBoss.java
@@ -1,14 +1,13 @@
+
+import java.util.ArrayList;
+
 public class ElectroBoss extends Adventurer {
   int photons, photonMax;
-  int dmgBuff;
-  double dmgAmp;
 
   public ElectroBoss(String name, int hp){
     super(name, hp);
     photonMax = 3;
     photons = 0;
-    dmgBuff = 0;
-    dmgAmp = 1.0;
   }
 
   public ElectroBoss(String name){
@@ -33,9 +32,30 @@ public class ElectroBoss extends Adventurer {
     photons = n;
   }
 
+  public String attack(ArrayList<Adventurer> enemies) {
+    int rand = (int) (Math.random() * enemies.size());
+    String ret = getName() + "used Thunder Pulse, dealing 3 damage to " + enemies.get(rand).getName() + " and 2 to the rest.";
+    for (int i = 0; i < enemies.size(); i++) {
+      if (i == rand) {
+        enemies.get(i).applyDamage(3);
+        int rand2 = (int) (Math.random() * 100);
+        if (rand2 < 50) {
+          enemies.get(i).setBurned(3);
+          ret += " " + enemies.get(i).getName() + "was also burned.";
+        } else if (rand2 < 75) {
+          enemies.get(i).setParalyzed(3);
+          ret += " " + enemies.get(i).getName() + "was also paralyzed.";
+        }
+      } else {
+        enemies.get(i).applyDamage(2);
+      }
+    }
+    return ret;
+  }
+
   public String attack(Adventurer other) {
     System.out.println("also remember to ask about continuous amp valuesremember to edit codewarrior to support extar effects likeburning/poison");
-
+    return "this is a placeholder func, ask mr k if you can change abstract attack parameters";
   }
 
   /*This is an example of an improvement that you can make to allow
@@ -45,8 +65,8 @@ public class ElectroBoss extends Adventurer {
   //public abstract String support(ArrayList<Adventurer> others);
 
   //heal or buff the target adventurer
-  public abstract String support(Adventurer other) {
-    this.support();
+  public String support(Adventurer other) {
+    return this.support();
   }
 
   //heal or buff self
