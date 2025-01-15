89ed87577c305622d183b4d19cedc8aaf1200cef
Henry Santos Hendricks <henrys8913@nycstudents.net>
2025-01-14 20:56:34 -0500

    added constructors

--- ElectroBoss.java
+++ ElectroBoss.java
@@ -1,15 +1,31 @@
 public class ElectroBoss extends Adventurer {
-  int charge;
+  int photons, photonMax;
+
+  public ElectroBoss(String name, int hp){
+    super(name, hp);
+    photonMax = 3;
+    photons = 0;
+  }
+
+  public ElectroBoss(String name){
+    this(name, 80);
+  }
+
+  public ElectroBoss(){
+    this("Potentious");
+  }
 
   public String getSpecialName() {
-    return "Charge";
+    return "Tainted Photons";
   }
   //accessor methods
-  public abstract int getSpecial() {
-    return charge;
+  public int getSpecial() {
+    return photons;
   }
-  public abstract int getSpecialMax() {
-    return 20;
+  public int getSpecialMax() {
+    return photonMax;
   }
-  public abstract void setSpecial(int n);
+  public void setSpecial(int n) {
+    photons = n;
+  }
 }
